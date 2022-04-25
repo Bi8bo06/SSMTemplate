9aabde17b9659b70cdf58e631143ce310e973a37
daishiqing <3166731909@qq.com>
2022-04-25 23:26:41 +0800

    Mybatis、Spring框架整合

---
+++ src/main/java/com/dsq/pojo/Books.java
@@ -0,0 +1,9 @@
+package com.dsq.pojo;
+
+
+public class Books {
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+}
---
+++ src/main/java/com/dsq/service/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package com.dsq.service;
+
+import com.dsq.dao.BookMapper;
+import com.dsq.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+    //service 调dao层
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
