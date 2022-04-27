e85e8ad38f8800e8d663ded6b20849ace6a7c62b
daishiqing <623722700@qq.com>
2022-04-27 13:57:07 +0800

    整合SpringMVC

--- src/main/java/com/dsq/pojo/Books.java
+++ src/main/java/com/dsq/pojo/Books.java
@@ -1,6 +1,12 @@
 package com.dsq.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Books {
     private int bookID;
     private String bookName;
