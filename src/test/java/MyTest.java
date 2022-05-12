f0fc5752bda8f5ec3a261ff8b9ecb4d7f080e62c
daishiqing <623722700@qq.com>
2022-05-12 15:40:00 +0800

    导入log4j日志

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,6 @@
 import com.dsq.pojo.Books;
 import com.dsq.service.BookService;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @description:
  */
 public class MyTest {
+    static Logger logger = Logger.getLogger(MyTest.class);
     @Test
     public void test(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -16,5 +18,12 @@ public class MyTest {
         for (Books books: bookServiceImpl.queryAllBook()){
             System.out.println(books);
         }
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info:进入的testLog4j");
+        logger.debug("debug:进入的testLog4j");
+        logger.error("error:进入的testLog4j");
     }
 }
